5ffce2c1c396c095333dd5a1b2190e44d8b435ee
pghuy <huygiaphi123@gmail.com>
2026-09-16 01:39:33 +0700

    Lab04_80%

---
+++ Lab04/Lab04_Model/Lab04_Model/Controllers/PeopleController.cs
@@ -0,0 +1,73 @@
+﻿using Lab04_Model.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Lab04_Model.Controllers
+{
+    public class PeopleController : Controller
+    {
+        // Hiển thị danh sách 
+        public IActionResult Index()
+        {
+            var _peoples = Models.DataLocal.GetPeoples();
+            return View(_peoples);
+        }
+
+        // Hiển thị đối tượng theo id 
+        // GET: /People/Details/5
+        public IActionResult Details(int id)
+        {
+            var _people = Models.DataLocal.GetPeopleById(id);
+            return View(_people);
+        }
+
+        // GET: Phương thức tạo đối tượng mới  
+        public IActionResult Create()
+        {
+            People people = new People();
+            return View(people);
+        }
+
+        // Phương thức Post: Tạo đối tượng mới
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(People model)
+        {
+            try
+            {
+                // 1. Lấy danh sách file được gửi lên từ Form (thông qua enctype="multipart/form-data")
+                var files = HttpContext.Request.Form.Files;
+
+                // 2. Kiểm tra xem người dùng có chọn file ảnh hay không
+                if (files.Count() > 0 && files[0].Length > 0)
+                {
+                    var file = files[0];
+                    var FileName = file.FileName;
+
+                    // Đường dẫn lưu file vật lý vào thư mục: wwwroot/images/avatar (hoặc avatars)
+                    var path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot\\images\\avatars", FileName);
+
+                    // Copy file ảnh vào ổ đĩa trên Server
+                    using (var stream = new FileStream(path, FileMode.Create))
+                    {
+                        file.CopyTo(stream);
+                    }
+
+                    // Gán đường dẫn tương đối của ảnh vào thuộc tính Avatar của Model
+                    model.Avatar = "images/avatars/" + FileName;
+                }
+
+                // 3. Thêm đối tượng People mới vào danh sách dữ liệu dùng chung DataLocal
+                DataLocal._peoples.Add(model);
+
+                // 4. Chuyển hướng về trang danh sách (Index)
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                // Nếu có lỗi thì giữ nguyên dữ liệu đã nhập và trả về lại View Create
+                return View(model);
+            }
+        }
+
+    }
+}
---
+++ Lab04/Lab04_Model/Lab04_Model/Models/DataLocal.cs
@@ -0,0 +1,82 @@
+﻿namespace Lab04_Model.Models
+{
+    public class DataLocal
+    {
+        public static List<People> _peoples = new List<People>()  // phải là static để có thể truy cập từ bên ngoài và không cần khởi tạo đối tượng
+        {
+            new People
+            {
+                Id = 1,
+                Name = "Nguyễn Văn Anh",
+                Email = "vananh@gmail.com",
+                Phone = "0912345671",
+                Address = "Hà Nội",
+                Avatar = "images/avatars/avt1.jpg",
+                Birthday = new DateTime(1998, 5, 15),
+                Bio = "Lập trình viên C#",
+                Gender = 1 // 1: Nam
+            },
+            new People
+            {
+                Id = 2,
+                Name = "Trần Thị Bình",
+                Email = "thibinh@gmail.com",
+                Phone = "0912345672",
+                Address = "Đà Nẵng",
+                Avatar = "images/avatars/avt2.jpg",
+                Birthday = new DateTime(2000, 8, 20),
+                Bio = "Thiết kế UI/UX",
+                Gender = 0 // 0: Nữ
+            },
+            new People
+            {
+                Id = 3,
+                Name = "Phạm Gia Cường",
+                Email = "giacuong@gmail.com",
+                Phone = "0912345673",
+                Address = "TP. Hồ Chí Minh",
+                Avatar = "images/avatars/avt3.jpg",
+                Birthday = new DateTime(1995, 12, 10),
+                Bio = "Quản lý dự án",
+                Gender = 1
+            },
+            new People
+            {
+                Id = 4,
+                Name = "Lê Thị Dung",
+                Email = "thidung@gmail.com",
+                Phone = "0912345674",
+                Address = "Hải Phòng",
+                Avatar = "images/avatars/avt4.jpg",
+                Birthday = new DateTime(2001, 3, 25),
+                Bio = "Chuyên viên Marketing",
+                Gender = 0
+            },
+            new People
+            {
+                Id = 5,
+                Name = "Hoàng Văn Em",
+                Email = "vanem@gmail.com",
+                Phone = "0912345675",
+                Address = "Cần Thơ",
+                Avatar = "images/avatars/avt5.jpg",
+                Birthday = new DateTime(1999, 11, 5),
+                Bio = "Kiểm thử phần mềm (Tester)",
+                Gender = 1
+            }
+        };
+
+        // Lấy danh sách dữ liệu 
+        public static List<People> GetPeoples()
+        {
+            return _peoples;
+        }
+
+        // Lấy đối tượng theo Id
+        public static People GetPeopleById(int id)
+        {
+            return _peoples.FirstOrDefault(p => p.Id == id);
+
+        }
+    }
+}
